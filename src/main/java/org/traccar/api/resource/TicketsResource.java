0c0cd5ea1983df6a24c51e802f34545c81bad7d7
patrick_ct <kcarrillo@software-ti.mx>
2024-01-28 17:57:17 -0600

    2024

--- src/main/java/org/traccar/api/resource/TicketsResource.java
+++ src/main/java/org/traccar/api/resource/TicketsResource.java
@@ -4,8 +4,12 @@
  */
 package org.traccar.api.resource;
 
+import java.time.Duration;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,10 +21,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.apache.commons.lang3.ObjectUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.traccar.api.BaseObjectResource;
-import org.traccar.api.SimpleObjectResource;
 import org.traccar.config.Config;
 import org.traccar.model.Device;
+import org.traccar.model.HojaSalida;
+import org.traccar.model.HoraSalida;
+import org.traccar.model.Itinerario;
 import org.traccar.model.Salida;
 import org.traccar.model.Ticket;
 import org.traccar.model.User;
@@ -100,5 +110,113 @@ public class TicketsResource extends BaseObjectResource<Ticket> {
         }
         return result;
 
+    }
+
+    @GET
+    @Path("/hoja")
+    public Response getHoja(
+            @QueryParam("deviceId") long deviceId,
+            @QueryParam("from") Date from,
+            @QueryParam("to") Date to) throws StorageException {
+
+        JSONObject obj = new JSONObject();
+        HojaSalida sheet = storage.getObject(HojaSalida.class, new Request(new Columns.All(), Condition.merge(new ArrayList<>() {
+            {
+                add(new Condition.Equals("valid", true));
+                add(new Condition.Between("day", "from", from, "to", to));
+            }
+        })
+        ));
+        if (sheet == null) {
+            Salida salida = storage.getObject(Salida.class, new Request(new Columns.All(), Condition.merge(new ArrayList<>() {
+                {
+                    add(new Condition.Equals("valid", true));
+                    add(new Condition.Equals("deviceId", deviceId));
+                    add(new Condition.Between("date", "from", from, "to", to));
+                }
+            })
+            ));
+
+            if (salida == null) {
+                obj.append("deviceId", 0);
+                obj.append("device", "");
+                return Response.ok(obj.toMap()).build();
+            }
+
+            sheet = new HojaSalida();
+            sheet.setDeviceId(deviceId);
+            sheet.setObservations("");
+            sheet.setScheduleId(salida.getScheduleId());
+            sheet.setDay(from);
+            sheet.setValid(true);
+            sheet.setId(storage.addObject(sheet, new Request(new Columns.Exclude("id"))));
+        }
+
+        obj.append("deviceId", deviceId);
+        obj.append("device", ((Device) ObjectUtils.defaultIfNull(cacheManager.getObject(Device.class, deviceId), storage.getObject(HojaSalida.class, new Request(new Columns.All(), Condition.merge(new ArrayList<>() {
+            {
+                add(new Condition.Equals("id", deviceId));
+            }
+        })
+        )))).getName());
+        obj.put("observations", sheet.getObservations());
+        final HojaSalida s = sheet;
+        Itinerario schedule = storage.getObject(Itinerario.class, new Request(new Columns.All(), Condition.merge(new ArrayList<>() {
+            {
+                add(new Condition.Equals("id", s.getScheduleId()));
+            }
+        })
+        ));
+        List<HoraSalida> horas_ida = cacheManager.getStorage().getObjects(HoraSalida.class, new Request(new Columns.All(), new Condition.Equals("name", cacheManager.getStorage().getObject(HoraSalida.class, new Request(new Columns.All(), new Condition.Equals("id", schedule.getHorasId()))).getName())));
+        Itinerario schedule_rel = storage.getObject(Itinerario.class, new Request(new Columns.All(), Condition.merge(new ArrayList<>() {
+            {
+                add(new Condition.Equals("horasIdRel", schedule.getHorasId()));
+            }
+        })
+        ));
+        List<HoraSalida> horas_vuelta = cacheManager.getStorage().getObjects(HoraSalida.class, new Request(new Columns.All(), new Condition.Equals("name", cacheManager.getStorage().getObject(HoraSalida.class, new Request(new Columns.All(), new Condition.Equals("id", schedule_rel.getHorasId()))).getName())));
+
+        List<Salida> salidas_ida = storage.getObjects(Salida.class, new Request(new Columns.All(), Condition.merge(new ArrayList<>() {
+            {
+                add(new Condition.Equals("valid", true));
+                add(new Condition.Equals("deviceId", deviceId));
+                add(new Condition.Between("date", "from", from, "to", to));
+                add(new Condition.Equals("scheduleId", schedule.getId()));
+            }
+        })
+        ));
+
+        List<Salida> salidas_vuelta = storage.getObjects(Salida.class, new Request(new Columns.All(), Condition.merge(new ArrayList<>() {
+            {
+                add(new Condition.Equals("valid", true));
+                add(new Condition.Equals("deviceId", deviceId));
+                add(new Condition.Between("date", "from", from, "to", to));
+                add(new Condition.Equals("scheduleId", schedule.getId()));
+            }
+        })
+        ));
+
+        obj.put("hours", new JSONArray());
+        for (int i = 0; i < horas_ida.size(); i++) {
+            final int index = i;  // Create a final variable
+            ((JSONArray) obj.get("hours")).put(new JSONObject() {
+                {
+                    put("forward", new JSONObject() {
+                        {
+                            append("expected", horas_ida.get(index) != null ? horas_ida.get(index).getHour() : null);
+                            append("difference", (salidas_ida.get(index) != null && salidas_ida.get(index) != null) ? Duration.between(horas_ida.get(index).getHour().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(), salidas_ida.get(index).getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) : null);
+                        }
+                    });
+                    put("backward", new JSONObject() {
+                        {
+                            append("expected", horas_vuelta.get(index) != null ? horas_vuelta.get(index).getHour() : null);
+                            append("difference", (salidas_vuelta.get(index) != null && salidas_vuelta.get(index) != null) ? Duration.between(horas_vuelta.get(index).getHour().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(), salidas_vuelta.get(index).getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) : null);
+                        }
+                    });
+                }
+            });
+        }
+
+        return Response.ok(obj.toMap()).build();
     }
 }
---
+++ src/main/java/org/traccar/model/HojaSalida.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.traccar.model;
+
+import java.util.Date;
+import org.traccar.storage.StorageName;
+
+/**
+ *
+ * @author USER
+ */
+@StorageName("tc_hojas")
+public class HojaSalida extends BaseModel {
+
+    private long deviceId;
+    private long scheduleId;
+    private Date day;
+    private String observations;
+    private boolean valid;
+
+    public long getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(long deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public long getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(long scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public boolean getValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+    
+}
